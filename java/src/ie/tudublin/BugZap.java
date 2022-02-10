bcc1a34203bdc3a5afdf215aeacfc9acf3b1761f
Xelqi <milek1093@gmail.com>
2022-02-10 18:19:48 +0000

    finished game

--- java/src/ie/tudublin/BugZap.java
+++ java/src/ie/tudublin/BugZap.java
@@ -2,60 +2,94 @@ package ie.tudublin;
 
 import processing.core.PApplet;
 
-public class BugZap extends PApplet
-{
+public class BugZap extends PApplet {
 
-    float playerX, playerY, playerWidth;
+	float playerX, playerY, playerWidth;
 
-	public void settings()
-	{
+	float bugX, bugY, bugWidth;
+
+	public void settings() {
 		size(500, 500);
-        
 
 	}
 
-	public void setup() 
-    {
-        colorMode(RGB);
+	public void setup() {
+		colorMode(RGB);
 
 		smooth();
 
-        playerX = width / 2;
-        playerY = height - 50;
-        playerWidth = 50;
+		playerX = width / 2;
+		playerY = height - 50;
+		playerWidth = 50;
+
+		resetBug();
 	}
-	
-    void drawPlayer(float x, float y, float w){
-        
-        stroke(255);
+
+	private void resetBug() {
+		bugX = random(bugWidth / 2, width - (bugWidth / 2));
+		bugY = 50;
+		bugWidth = 50;
+	}
+
+	void drawBug(float x, float y, float w) {
+
+		float halfW = w / 2;
+		stroke(255);
+		noFill();
+		triangle(x - halfW, y + halfW, x, y, x + halfW, y + halfW);
+	}
+
+	void drawPlayer(float x, float y, float w) {
+
+		stroke(255);
 		noFill();
 		rectMode(CENTER);
 		rect(x, y, w, 20);
-        line(x, y - 10, x, y - 20);
+		line(x, y - 10, x, y - 20);
 
-    }
-
-	public void draw()
-	{	
-		background(0);
-		strokeWeight(2);
-        drawPlayer(playerX, playerY, playerWidth);
 	}
 
-    public void keyPressed()
-	{
-		if (keyCode == LEFT)
-		{
-			System.out.println("Left arrow pressed");
-           
+	void moveBug() {
+		bugY++;
+		bugX += random(-20, 20);
+	}
+
+	public void draw() {
+		background(0);
+		strokeWeight(2);
+		drawPlayer(playerX, playerY, playerWidth);
+		drawBug(bugX, bugY, bugWidth);
+		if (frameCount % 20 == 0) {
+			moveBug();
 		}
-		if (keyCode == RIGHT)
-		{
-			System.out.println("Right arrow pressed");
+		text("Score: " + score, 50, 50);
+	}
+
+	float playerSpeed = 5;
+
+	public void keyPressed() {
+		if (keyCode == LEFT) {
+			playerX -= playerSpeed;
+
 		}
-		if (key == ' ')
-		{
-			System.out.println("SPACE key pressed");
+		if (keyCode == RIGHT) {
+
+			playerX += playerSpeed;
+
 		}
-	}	
+		if (key == ' ') {
+
+			float halfW = width / 2;
+			if (playerX > bugX - halfW && playerX < bugX + halfW) {
+
+				score++;
+				resetBug();
+				line(playerX, playerY - 10, playerX, bugY);
+			} else {
+				line(playerX, playerY - 10, playerX, 0);
+			}
+		}
+	}
+
+	int score = 0;
 }
