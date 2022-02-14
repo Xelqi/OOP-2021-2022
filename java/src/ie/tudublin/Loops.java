bc618c399a2890732ddc3c1d9b85b34544ebb0d4
Xelqi <milek1093@gmail.com>
2022-02-14 11:56:29 +0000

    fixing code

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -5,9 +5,6 @@ import processing.core.PApplet;
 public class Loops extends PApplet
 {
 
-
-	int mode = 0;
-
 	public void settings()
 	{
 		size(500, 500);
@@ -18,63 +15,8 @@ public class Loops extends PApplet
 		
 	}
 
-	public void keyPressed()
-	{
-		if (key >= '0' && key <='9')
-		{
-			mode = key - '0';
-		}
-		println(mode);
-	}
-
-	float magicMap(float a, float b, float c, float d, float e)
-	{
-		float output;
-		a -= b;
-		c -= b;
-		e-= d;
-
-		output = ((a/c)*e) + d;
-
-		return output;
-	}
-
-	float magicMap1(float a, float b, float c, float d, float e)
-	{
-		float r1 = c -b;
-		float r2 = e-d;
-		float howFar = a - b;
-
-		return d + ((howFar / r1) * r2);
-	}
-
 	
 	public void draw()
-	{
-		
-		switch(mode)	
-		{
-			case 0:
-				background(0);
-				int bars = (int) (mouseX / 20.0f);
-				float w = width / (float)bars;	
-				for(int i = 0 ; i < bars ; i ++)
-				{
-					noStroke();
-					fill(map(i, 0, bars, 0, 255), 255, 255);
-					rect(map(i, 0, bars, 0, 500), 0, w, height);
-				}
-				break;
-			case 1:
-			
-				break;
-				//map(a,b,c,d,e);
-				//a = inputvalue
-				// b - c - start and end of the first range
-				// d, e 0 - start and and of the end range
-
-				// map(-2, 10, 90, 200, 233);
-
-		}
+	{	
 	}
 }
